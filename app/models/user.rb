3fa1308d65670d237fec29d23a1048a54f98b84c
ss14527 <shriya.singhania@carleton.ca>
2020-06-13 14:26:21 -0400

    setup db and added models students, users, course & enrollment

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  #authenticate password
+  has_secure_password
+
+  #basic password validation
+  validates_length_of       :password, maximum: 72, minimum: 8, allow_nil: false, allow_blank: false
+  validates_confirmation_of :password, allow_nil: false, allow_blank: false
+
+  #ensure username and email are unique and present
+  validates_presence_of     :email
+  validates_presence_of     :username
+  validates_uniqueness_of   :email
+  validates_uniqueness_of   :username
+
+  # This method gives us a simple call to check if a user has permission to modify.
+  def can_modify_user?(user_id)
+    role == 'admin' || id.to_s == user_id.to_s
+  end
+
+  # This method tells us if the user is an admin or not.
+  def is_admin?
+    role == 'admin'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'courses/index'
+  post 'user_token' => 'user_token#create'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
